a02847a1f8a5551bdb2179a12e824757c7da53c5
Andrew Tolochkevich <tolochkevichdev@gmail.com>
2017-09-27 15:30:59 +0300

    map->links

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('#start_money a').attr('href','https://www.liqpay.ua/ru/checkout/card/i86322090701');
+    $('#give_some_money a').attr('href','https://www.liqpay.ua/ru/checkout/card/i86322090701');
+    $('#give_money a').attr("href","https://www.liqpay.ua/api/3/checkout?data=eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXlkb25hdGUiLCJwdWJsaWNfa2V5IjoiaTg2MzIyMDkwNzAxIiwiYW1vdW50IjoiMTAwMCIsImN1cnJlbmN5IjoiVVNEIiwiZGVzY3JpcHRpb24iOiLQptC10LPQu9C40L3QsCDQsiDQvtGB0L3QvtCy0YMg0YXRgNCw0LzRgyDQlNC20LDQvdCz0YfRg9CxINCn0L7Qu9GW0L3SkSIsInR5cGUiOiJkb25hdGUiLCJsYW5ndWFnZSI6InJ1In0%3D&signature=ooFrP3JbbYAhC5bouOxROh1eMaU%3D");
+});
